209e5539fd588bc3b4bb19901a2f50c255a2b9f9
ofreshy <sharoffer@gmail.com>
2017-05-21 22:39:08 +0100

    Adding own validators

--- vast/models/__init__.py
+++ vast/models/__init__.py
@@ -1,9 +1,4 @@
-from attr import validators
 
-STR_VALIDATOR = validators.instance_of(str)
-INT_VALIDATOR = validators.instance_of(int)
-BOOL_VALIDATOR = validators.instance_of(bool)
-IN_VALIDATOR = validators.in_
 
 
 
