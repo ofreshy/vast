1a1b2f5c6a1d8cce932ff15047c2791ed5893551
ofreshy <sharoffer@gmail.com>
2017-05-21 22:02:28 +0100

    Adding models and specs

---
+++ vast/models/__init__.py
@@ -0,0 +1,10 @@
+from attr import validators
+
+STR_VALIDATOR = validators.instance_of(str)
+INT_VALIDATOR = validators.instance_of(int)
+BOOL_VALIDATOR = validators.instance_of(bool)
+IN_VALIDATOR = validators.in_
+
+
+
+
