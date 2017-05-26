b277080cbc569470784b975912955984ecb08ad9
ofreshy <sharoffer@gmail.com>
2017-05-26 17:41:34 +0100

    Working order

--- vast/parsers/vast_v2.py
+++ vast/parsers/vast_v2.py
@@ -1,176 +1,147 @@
-from collections import defaultdict
+import attr
 
 from vast.models.vast_v2 import Ad, Vast, Wrapper
 from vast.parsers import ParseError
 
 
+@attr.s()
+class _ParsedVast(object):
+    vast = attr.ib()
+    version = attr.ib()
+    ad = attr.ib()
+
+    @classmethod
+    def from_xml_dict(cls, xml_dict):
+        vast = xml_dict.get("VAST")
+
+        error_msg = None
+        if not vast:
+            error_msg = "Must declare a vast element in xml dict - '%s'"
+        elif "@version" not in vast:
+            error_msg = "Must declare version attribute in vast - '%s'"
+        elif vast["@version"] != "2.0":
+            error_msg = "Called vast 2.0 parser on vast with different version-  '%s'"
+        elif "Ad" not in vast:
+            error_msg = "Must declare Ad element in vast - '%s'"
+
+        if error_msg is not None:
+            raise ParseError(error_msg % xml_dict)
+
+        version = vast["@version"]
+        ad = _ParsedAd.from_xml_dict(vast["Ad"])
+
+        return cls(vast, version, ad)
+
+
+@attr.s()
 class _ParsedAd(object):
-    def __init__(self, ad):
-        self.ad = ad
+    ad = attr.ib()
+    inline = attr.ib()
+    wrapper = attr.ib()
+
+    @classmethod
+    def from_xml_dict(cls, xml_dict):
+        inline, wrapper = None, None
+        if "InLine" in xml_dict:
+            inline = _ParsedInLine.from_xml_dict(xml_dict["Inline"])
+        elif "Wrapper" in xml_dict:
+            wrapper = _ParsedWrapper.from_xml_dict(xml_dict["Wrapper"])
+        else:
+            msg = "Must declare either inline or wrapper in xml_dict '%s'"
+            raise ParseError(msg % xml_dict)
+
+        return cls(xml_dict, inline, wrapper)
 
     @property
     def id(self):
         return self.ad.get("@id")
 
-    @property
-    def inline(self):
-        return self.ad.get("Inline")
 
-    @property
-    def wrapper(self):
-        return self.ad.get("Wrapper")
-
-
+@attr.s()
 class _ParsedWrapper(object):
-    def __init__(self, wrapper_dict):
-        self.wrapper_dict = wrapper_dict
+    wrapper = attr.ib()
+
+    @classmethod
+    def from_xml_dict(cls, xml_dict):
+        return cls(xml_dict)
 
     @property
     def vast_ad_tag_uri(self):
-        return self.wrapper_dict.get("VASTAdTagURI")
+        return self.wrapper.get("VASTAdTagURI")
 
     @property
     def ad_system(self):
-        return self.wrapper_dict.get("AdSystem")
+        return self.wrapper.get("AdSystem")
 
     @property
     def ad_title(self):
-        return self.wrapper_dict.get("AdTitle")
+        return self.wrapper.get("AdTitle")
 
     @property
     def impression(self):
-        return self.wrapper_dict.get("Impression")
+        return self.wrapper.get("Impression")
 
     @property
     def error(self):
-        return self.wrapper_dict.get("Error")
+        return self.wrapper.get("Error")
 
     @property
     def creatives(self):
-        creatives = self.wrapper_dict.get("creatives", {})
+        creatives = self.wrapper.get("creatives", {})
         for v in creatives.values():
             yield v
 
 
+@attr.s()
 class _ParsedInLine(object):
-    def __init__(self, root):
-        self._root = root
-        ad = root["VAST"].get("Ad", {})
-        self._inline = ad.get("InLine", {})
+    inline = attr.ib()
 
-    @property
-    def mimetypes(self):
-        mimetypes = set([])
-        for media_file in self.media_files:
-            mimetype = media_file["@type"]
-            if mimetype not in mimetypes:
-                yield mimetype
-            mimetypes.add(mimetype)
-
-    @property
-    def mime_to_bitrates(self):
-        mime_to_bitrates = defaultdict(list)
-        for media_file in self.media_files:
-            mimetype = media_file["@type"]
-            bitrate = media_file.get("@bitrate")
-            if bitrate:
-                mime_to_bitrates[mimetype].append(int(bitrate))
-
-        for mime_type, bitrates in mime_to_bitrates.items():
-            mime_to_bitrates[mime_type] = sorted(bitrates)
-
-        return mime_to_bitrates
-
-    @property
-    def mime_to_sizes(self):
-        mime_to_sizes = defaultdict(set)
-        for media_file in self.media_files:
-            mimetype = media_file["@type"]
-            width, height = media_file.get("@width"), media_file.get("@height")
-            if width and height:
-                size = (int(width), int(height))
-                mime_to_sizes[mimetype].add(size)
-
-        for mime_type, values in mime_to_sizes.items():
-            mime_to_sizes[mime_type] = sorted(list(values))
-
-        return mime_to_sizes
-
-    @property
-    def creatives(self):
-        # only interested in Linear right now
-        creatives = self._inline.get("Creatives", {})
-        for v in creatives.values():
-            if "Linear" in v:
-                yield v["Linear"]
-
-    @property
-    def media_files(self):
-        for creative in self.creatives:
-            for media_file in creative.get("MediaFiles", {}).get("MediaFile"):
-                yield media_file
-
-    @property
-    def durations(self):
-        for creative in self.creatives:
-            yield creative["Duration"]
-
-    @property
-    def duration(self):
-        return next(self.durations, None)
+    @classmethod
+    def from_xml_dict(cls, xml_dict):
+        return cls(xml_dict)
 
 
-def parse_xml(parsed_vast):
-    version = parsed_vast.version
-    ad = _ParsedAd(parsed_vast.ad)
-
-    if ad.wrapper:
+def parse_xml(root):
+    vast = _ParsedVast.from_xml_dict(root)
+    ad = vast.ad
+    wrapper = ad.wrapper
+    inline = ad.inline
+    if wrapper:
         return Vast.make(
-            version=version,
+            version=vast.version,
             ad=Ad.make(
                 id=ad.id,
-                wrapper=_parse_wrapper(ad),
+                wrapper=Wrapper.make(
+                    ad_system=wrapper.ad_system,
+                    vast_ad_tag_uri=wrapper.vast_ad_tag_uri,
+                    ad_title=wrapper.ad_title,
+                    impression=wrapper.impression,
+                    error=wrapper.error,
+                    creatives=creatives_from_wrapper(wrapper),
+                ),
             ),
         )
-    elif ad.inline:
+    elif inline:
         return Vast.make(
-            version=version,
+            version=vast.version,
             ad=Ad.make(
                 id=ad.id,
-                inline=_parse_inline(ad),
+                inline=_parse_inline(inline),
             ),
         )
     else:
-        raise ParseError("Must provide either Wrapper or Inline element in ad '%s'" % parsed_vast.ad)
+        raise ParseError("This should never be called but here just in case")
 
 
-def _parse_wrapper(ad):
-    wrapper = _ParsedWrapper(ad.wrapper)
-    ad_system = wrapper.ad_system
-    vast_ad_tag_uri = wrapper.vast_ad_tag_uri
-
-    ad_title = wrapper.ad_title
-    impression = wrapper.impression
-    error = wrapper.error
-
-    creatives = [from_creaive_dict(c) for c in wrapper.creatives] or None
-
-    wrapper_model = Wrapper.make(
-        ad_system=ad_system,
-        vast_ad_tag_uri=vast_ad_tag_uri,
-        ad_title=ad_title,
-        impression=impression,
-        error=error,
-        creatives=creatives,
-    )
-    return wrapper_model
-
-
-def _parse_inline(ad):
-    inline = _ParsedInLine(ad.inline)
+def _parse_inline(inline):
     return inline
 
 
 def from_creaive_dict(c_dict):
     return None
+
+
+def creatives_from_wrapper(wrapper):
+    return [from_creaive_dict(c) for c in wrapper.creatives] or None
+
 
--- vast/parsers/xml_parser.py
+++ vast/parsers/xml_parser.py
@@ -1,5 +1,6 @@
 import xmltodict
 
+from vast.parsers import ParseError
 
 from vast.parsers import vast_v2
 
@@ -8,26 +9,20 @@ _PARSERS = {
 }
 
 
-class _ParsedVast(object):
-    def __init__(self, root):
-        self.root = root
-
-    @property
-    def version(self):
-        return self.root["VAST"]["@version"]
-
-    @property
-    def ad(self):
-        return self.root["VAST"].get("Ad", {})
-
-
 def from_xml_string(xml_input, **kwargs):
     root = xmltodict.parse(xml_input, **kwargs)
+    if "VAST" not in root:
+        raise ParseError("root must have VAST element")
+    vast = root["VAST"]
 
-    vast = _ParsedVast(root)
-    version = vast.version
+    if not isinstance(vast, dict):
+        raise ParseError("vast must have children elements but was '%s'" % vast)
+    version = vast.get("@version")
+    if not version:
+        raise ParseError("missing version attribute in vast element '%s'" % vast)
+
     parser = _PARSERS.get(version)
     if parser is None:
-        raise ValueError("unknown version %s" % version)
+        raise ParseError("Cannot parse vast version %s" % version)
 
-    return parser(vast)
+    return parser(root)
