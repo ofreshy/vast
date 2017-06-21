599cc5dc548b34d8c0be9fdd21c75c7dfa7b7826
ofreshy <sharoffer@gmail.com>
2017-06-21 14:12:29 +0100

    Adding tests for valid cases

--- vast/models/tests/test_vast_v2.py
+++ vast/models/tests/test_vast_v2.py
@@ -1,5 +1,7 @@
 import unittest
 
+from testscenarios import TestWithScenarios
+
 from vast.models import vast_v2
 from vast.models.tests.vast_v2_model_mixin import VastModelMixin
 
@@ -22,6 +24,26 @@ class TestAsDict(unittest.TestCase):
         print linear.as_dict()
 
 
+class TestVastMixinDefaultsWork(VastModelMixin, TestWithScenarios):
+    scenarios = [
+        ("make_vast", dict(make_func="make_vast")),
+        ("make_wrapper_ad", dict(make_func="make_wrapper_ad")),
+        ("make_inline_ad", dict(make_func="make_inline_ad")),
+        ("make_wrapper", dict(make_func="make_wrapper")),
+        ("make_inline", dict(make_func="make_inline")),
+        ("make_creatives", dict(make_func="make_creatives")),
+        ("make_creative", dict(make_func="make_creative")),
+        ("make_linear_creative", dict(make_func="make_linear_creative")),
+        ("make_media_files", dict(make_func="make_media_files")),
+        ("make_media_file", dict(make_func="make_media_file")),
+        ("make_tracking_event", dict(make_func="make_tracking_event")),
+    ]
+
+    def test_it_makes_it(self):
+        try:
+            getattr(self, self.make_func)()
+        except Exception as e:
+            self.fail("make func failed,  %s" % e)
 
 class TestMakeVast(VastModelMixin, unittest.TestCase):
     def test_valid_passes(self):
@@ -48,6 +70,3 @@ class TestLinearCreative(VastModelMixin, unittest.TestCase):
             self.make_linear_creative()
         except ValueError as e:
             self.fail("Failed to make tracking_event, %s" % e)
-
-
-
--- vast/models/tests/vast_v2_model_mixin.py
+++ vast/models/tests/vast_v2_model_mixin.py
@@ -2,6 +2,11 @@ from vast.models import vast_v2
 
 
 class VastModelMixin(object):
+    def make_vast(self, version=u"2.0", ad=None):
+        return vast_v2.Vast.make(
+            version=version,
+            ad=ad or self.make_wrapper_ad(),
+        )
     def make_wrapper_ad(self, wrapper=None):
         return vast_v2.Ad.make_wrapper(
             id="ad_wrapper_id",
@@ -12,6 +17,58 @@ class VastModelMixin(object):
         return vast_v2.Ad.make_inline(
             id="ad_inline_id",
             inline=inline or self.make_inline(),
+        )
+
+    @staticmethod
+    def make_wrapper(
+            ad_system=u"Mag",
+            vast_ad_tag_uri=u"https://www.magu.com",
+            ad_title=u"MagAd",
+            impression=u"https://www.mag_impression.com",
+            error=u"https://www.mag_error.com",
+            creatives=None,
+    ):
+        return vast_v2.Wrapper.make(
+            ad_system=ad_system,
+            vast_ad_tag_uri=vast_ad_tag_uri,
+            ad_title=ad_title,
+            impression=impression,
+            error=error,
+            creatives=creatives,
+        )
+
+    def make_inline(
+            self,
+            ad_system=u"Mag",
+            ad_title=u"MagAd",
+            impression=u"https://www.mag_impression.com",
+            creatives=None,
+    ):
+        return vast_v2.Inline.make(
+            ad_system=ad_system,
+            ad_title=ad_title,
+            impression=impression,
+            creatives=creatives or self.make_creatives()
+        )
+
+    def make_creatives(self):
+        return [self.make_creative()]
+
+
+    def make_creative(
+            self,
+            linear=None, non_linear=None, companion_ads=None,
+            id="creative_id", sequence=None, ad_id="ad_id",
+            api_framework=vast_v2.ApiFramework.VPAID,
+    ):
+        return vast_v2.Creative.make(
+            linear=linear or self.make_linear_creative(),
+            non_linear=non_linear,
+            companion_ads=companion_ads,
+            id=id,
+            sequence=sequence,
+            ad_id=ad_id,
+            api_framework=api_framework,
         )
 
     @staticmethod
@@ -57,22 +114,4 @@ class VastModelMixin(object):
             asset, delivery, type, width, height,
             codec, id, bitrate, min_bitrate=None, max_bitrate=None,
             scalable=None, maintain_aspect_ratio=None, api_framework=None
-        )
-
-    @staticmethod
-    def make_wrapper(
-            ad_system=u"Mag",
-            vast_ad_tag_uri=u"https://www.magu.com",
-            ad_title=u"MagAd",
-            impression=u"https://www.mag_impression.com",
-            error=u"https://www.mag_error.com",
-            creatives=None,
-    ):
-        return vast_v2.Wrapper.make(
-            ad_system=ad_system,
-            vast_ad_tag_uri=vast_ad_tag_uri,
-            ad_title=ad_title,
-            impression=impression,
-            error=error,
-            creatives=creatives,
         )
