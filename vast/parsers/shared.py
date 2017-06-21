5d0d5c91d48b12e97d7241dd7f7c57a0bdd6c69f
ofreshy <sharoffer@gmail.com>
2017-06-21 20:45:16 +0100

    deletion of code

--- vast/parsers/shared.py
+++ vast/parsers/shared.py
@@ -40,33 +40,3 @@ def accept_falsy(parse_func):
         return parse_func(xml_dict)
 
     return parse
-
-
-def extract_fields(xml_dict, fields, method="required"):
-    if method == "required":
-        _check_required_fields(xml_dict, fields)
-    elif method == "one_of":
-        _check_one_of_fields(xml_dict, fields)
-    # else it is optional
-    return (xml_dict.get(f) for f in fields)
-
-
-def _check_one_of_fields(xml_dict, keys):
-    found = [k for k in keys if k in xml_dict]
-    if len(found) != 1:
-        msg = "Only 1 key from {keys} should be found, but found {found} in {xml_dict}"
-        raise ParseError(msg.format(keys=keys, found=found, xml_dict=xml_dict))
-
-
-def _check_required_fields(xml_dict, fields):
-    missing = [f for f in fields if not xml_dict.get(f)]
-    if missing:
-        msg = "Missing required fields : {missing} in {xml_dict}"
-        raise ParseError(msg.format(missing=missing, xml_dict=xml_dict))
-
-
-def to_bool(value):
-    value = str(value).lower()
-    if value in ("none", "0", "false"):
-        return False
-    return True
